8d953ef6c53054d4c8fea53f84c6b3b7b2164332
Larissa Goularte <58703854+larissagoularte@users.noreply.github.com>
2024-07-29 21:45:53 +0100

    add mongoose connection

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const url = process.env.DATABASE;
+
+async function connectDB() {
+    try {
+        await mongoose.connect(url);
+        console.log('DB Connect - OK');
+    } catch (err) {
+        console.error('DB Connect - FAIL');
+        console.error(err);
+    }
+}
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+require('dotenv').config();
+
+const connectDB = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello world :)');
